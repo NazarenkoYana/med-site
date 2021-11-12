007fd7d3ce20985f0e389bb3ec445287816bce7b
Yana.Nazarenko <yana.sopka95@gmail.com>
2021-11-12 17:21:11 +0200

    .

---
+++ src/js/info-preparation-slider.js
@@ -0,0 +1,19 @@
+sliderPrpducts = tns({
+    container: '.info-preparation__mobile',
+    items: 1,
+    mouseDrag: true,
+    swipeAngle: false,
+    center: true,
+    edgePadding: 0,
+    fixedWidth: 270,
+    gutter: 30,
+    nav: true,
+    controls: false,
+    responsive: {
+       375: {
+            gutter: 80,
+            items: 1
+        },
+    }
+
+});
---
+++ src/js/video-gallery-slider.js
@@ -0,0 +1,19 @@
+sliderPrpducts = tns({
+    container: '.video-gallery__container-content-mobile',
+    items: 1,
+    mouseDrag: true,
+    swipeAngle: false,
+    center: true,
+    edgePadding: 0,
+    // fixedWidth: 270,
+    gutter: 0,
+    nav: true,
+    controls: false,
+    responsive: {
+       375: {
+            gutter: 60,
+            items: 1
+        },
+    }
+
+});
