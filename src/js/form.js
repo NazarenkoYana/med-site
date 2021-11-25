afd54094971a9a1bba814c82542993d32603ec29
NazarenkoYana <67002128+NazarenkoYana@users.noreply.github.com>
2021-11-25 16:21:39 +0200

    Add files via upload
    .

--- src/js/form.js
+++ src/js/form.js
@@ -2,13 +2,20 @@ const enter = document.querySelector('.header__link-active-mobile');
 const btnClose = document.querySelector('.form-close')
 const btnRegister = document.querySelector('.btn-register');
 const headerActive = document.querySelector('.header__link-active');
-
+// scrollBody
 enter.addEventListener("click", scrollBody);
 btnClose.addEventListener("click", scrollBody);
 btnRegister.addEventListener("click", scrollBody);
 headerActive.addEventListener("click", scrollBody);
+//openForm
+enter.addEventListener("click", openForm);
+headerActive.addEventListener("click", openForm);
+btnRegister.addEventListener("click", openForm);
+//closeForm
+btnClose.addEventListener("click", closeForm);
 
 
+//
 function scrollBody() {
     if (document.body.classList.contains("stop-scrolling")) {
         document.body.classList.remove("stop-scrolling");
