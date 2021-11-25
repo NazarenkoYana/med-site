3436e78ca8cec88c2a0d125761a2730d62f41892
NazarenkoYana <67002128+NazarenkoYana@users.noreply.github.com>
2021-11-25 12:29:55 +0200

    Add files via upload
    .

--- src/js/form.js
+++ src/js/form.js
@@ -1,3 +1,22 @@
+const enter = document.querySelector('.header__link-active-mobile');
+const btnClose = document.querySelector('.form-close')
+const btnRegister = document.querySelector('.btn-register');
+const headerActive = document.querySelector('.header__link-active');
+
+enter.addEventListener("click", scrollBody);
+btnClose.addEventListener("click", scrollBody);
+btnRegister.addEventListener("click", scrollBody);
+headerActive.addEventListener("click", scrollBody);
+
+
+function scrollBody() {
+    if (document.body.classList.contains("stop-scrolling")) {
+        document.body.classList.remove("stop-scrolling");
+    } else {
+        document.body.classList.add("stop-scrolling");
+    }
+}
+
 function openForm() {
     document.querySelector('#myForm').style.display = "block";
 }
@@ -5,5 +24,3 @@ function openForm() {
 function closeForm() {
     document.querySelector("#myForm").style.display = "none";
 }
-
-
